087f0f8028a0f366a354a5f5c7661149bb3fab16
mbouin <mbouin@bioproj.invalid>
2013-07-09 14:30:18 +0000

    versionQT without use BDD
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/soma/soma-base/trunk@56975 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ python/soma/sandbox/QtNotationGUI/filter_window.py
@@ -0,0 +1,182 @@
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+import csv
+import re
+import glob
+import os
+
+class MyFilter(QtGui.QDialog):
+    def __init__(self,dirname):
+        super(MyFilter, self).__init__()
+        self.dirname_bdd='/neurospin/cati/Memento/MEMENTO_fevrier2013/geoCorrected'
+        self.dirname_snap=dirname       
+        
+        self.list_subject=''
+        #Files and marks in the data.csv  
+        self.filenames = []
+        self.marks = []  
+        self.nb_subject=0
+
+
+        #To write in the .txt
+        self.sign_filter_str=''
+        self.number=0
+        self.word_to_find=''
+ 
+        self.hbox = QtGui.QHBoxLayout()
+        self.hbox2=QtGui.QHBoxLayout()
+        self.vbox = QtGui.QVBoxLayout()    
+          
+        self.display_dirnam=QtGui.QLabel(self.dirname_snap)
+        self.choice_type=QtGui.QComboBox()
+        self.choice_type.addItems(['split','GW','hemi'])
+        self.choice_sign=QtGui.QComboBox()
+        self.choice_sign.addItems(['<','<=','=','>=','>'])
+        self.choice_number=QtGui.QComboBox()
+        self.choice_number.addItems(['0','1','2','3','4','5'])
+        self.hbox.addWidget(self.display_dirnam)
+        self.hbox.addWidget(self.choice_type)
+        self.hbox.addWidget(self.choice_sign)
+        self.hbox.addWidget(self.choice_number)
+        #self.groupbox.setLayout(self.hbox)
+        self.signals()
+        #self.hbox.addLayout( self.vbox)
+        #self.hbox.addStretch(1)
+        self.hbox2.addWidget(QtGui.QLabel('    Results filter'))
+        self.text_res=QtGui.QLabel()
+        self.hbox2.addWidget(self.text_res)
+        #self.hbox2.addWidget(self.label_res)
+        #self.hbox2.addWidget(self.text_res)
+        
+        self.vbox.addLayout(self.hbox)
+        self.vbox.addLayout(self.hbox2)
+        
+        self.button_save_quit=QtGui.QPushButton('Save&Quit')
+        self.button_save_quit.clicked.connect(self.save_quit)
+        self.vbox.addWidget(self.button_save_quit)
+        self.vbox.setAlignment(QtCore.Qt.AlignLeft)  
+        #self.vbox.addLayout(self.hbox2)
+        self.setLayout(self.vbox)
+        
+        
+     
+    def signals(self):    
+       self.choice_type.currentIndexChanged.connect(self.filter_process)   
+       self.choice_sign.currentIndexChanged.connect(self.filter_process)   
+       self.choice_number.currentIndexChanged.connect(self.filter_process)   
+        
+  
+    def get_files_and_marks(self): 
+        print 'IN GET FILE'
+        print self.dirname_snap 
+        data_file_name = os.path.join(self.dirname_snap,'data.csv')
+        print data_file_name
+        try:
+            with open(data_file_name, 'rb') as csvfile:
+                myreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+                for row in myreader:
+                    self.filenames.append(row[0])
+                    self.marks.append(row[1])
+        except IOError:
+            print 'IOERROR'
+            pass
+
+      
+    def filter_process(self):
+        print 'in filtre_process'
+        print self.dirname_snap
+        self.list_subject=''
+        self.nb_subject=0
+        self.word_in_the_file=str(self.choice_type.currentText())
+        print 'type file',self.word_in_the_file
+        if not self.filenames:
+            print 'OPEN CSV'
+            #if data.csv has not been opened
+            self.get_files_and_marks()
+        compteur=0 
+        index=0
+        for file in self.filenames:
+            if self.word_in_the_file in file:
+                compteur=compteur+1
+                sign=self.choice_sign.currentText()
+                self.number=self.choice_number.currentIndex()
+                if len(self.marks[index])>2:
+                    if self.comparison(sign,self.number,int(self.marks[index].split(' ')[0]))==1:
+                        self.raw_data(file)
+                else:
+                    if self.comparison(sign,self.number,int(self.marks[index]))==1:
+                        self.raw_data(file)
+                            
+            index=index+1 
+        self.text_res.setText(str(self.nb_subject))
+    
+    def write_results(self):
+        with open(os.path.join(self.dirname_snap,'results_filter_%s_%s_%d'%(self.word_in_the_file,self.sign_filter_str,self.number)), 'w') as f:
+            f.write(self.list_subject)       
+        
+
+    def raw_data(self,file):  
+        print 'raw_data'   
+        filename=file.split('.')[0]
+        expresion=r"([0-9]{7})([A-Za-z]{4})"
+        m=re.search(expresion, filename)
+        if m is not None:
+            subject=m.group(0)
+            filespliter=filename.split('_')
+            for i in range(0,len(filespliter)):
+                if filespliter[i]==subject:
+                    index=i
+                    break                     
+            nom_center=filespliter[index-1]
+            if nom_center.isdigit():
+                nom_center=filespliter[index-2]+'_'+filespliter[index-1]
+        
+        path=glob.glob(os.path.join(self.dirname_bdd,'%s' % nom_center,'%s' %subject,'*','*','%s.nii.gz'%subject))
+        if len(path)==0:
+            print 'ERROR NO RAW DATA FIND'
+            return           
+        else:
+            if len(path)>1:
+                print 'WARNING MORE THAN ON RAW DATA HAVE BEEN FOUND BY DEFAULT IT S %s'%path[0]
+            self.list_subject=self.list_subject+"'"+path[0]+"'"+' '   
+            self.nb_subject=self.nb_subject+1
+                
+    def comparison(self,sign,number,number_file):
+        if sign == '<':
+            self.sign_filter_str='inf'
+            if number_file < number:
+                return 1
+            else:
+                return 0
+        elif sign == '<=':
+            self.sign_filter_str='inf_equal'
+            if number_file <= number:
+                return 1
+            else:
+                return 0
+                
+        elif sign == '=':
+            self.sign_filter_str='equal'
+            if number_file == number:
+                return 1
+            else:
+                return 0     
+                  
+        elif sign == '>=':
+            self.sign_filter_str='sup_equal'
+            if number_file >= number:
+                return 1
+            else:
+                return 0    
+            
+        elif sign == '>':
+            self.sign_filter_str='sup'
+            if number_file > number:
+                return 1
+            else:
+                return 0               
+        
+
+    def save_quit(self):
+        print 'SAVE AND QUIT FILTER'
+        self.write_results()
---
+++ python/soma/sandbox/QtNotationGUI/main_window.py
@@ -0,0 +1,302 @@
+# -*- coding: utf-8 -*-
+
+
+#from PyQt4.QtGui import QMainWindow,QHBoxLayout,QVBoxLayout,QPushButton,QWidget,QComboBox,\
+#QLabel,QTextEdit,QGraphicsScene,QGraphicsView,QGraphicsPixmapItem,QPixmap,QAction,QFileDialog,\
+#QStatusBar,QStyle,QIcon,QImage
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+#from PyQt4.QtCore import QObject,QString,Qt.KeepAspectRatio,Qt.SmoothTransformation
+import os
+import read_csv
+
+from filter_window import MyFilter
+
+
+class MainWindow(QtGui.QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.data=read_csv.Data()   
+        self.data_file_name= None
+        self.dirname=None
+        self.pictures_in_directory=None
+        self.index_picture_display=None
+        self.image_brain=None
+        self.filter_window=MyFilter(self.dirname)
+        #self.filter_window.setParent(self)
+        
+        self.create_actions()
+        self.create_gui()
+        self.create_menu()
+
+    def create_gui(self):
+        self.MainWidget=QtGui.QWidget()
+        self.resize(800,600)
+        self.vbox = QtGui.QVBoxLayout()
+        self.hbox = QtGui.QHBoxLayout()        
+        self.button_prev=QtGui.QPushButton('<')
+        self.button_next=QtGui.QPushButton('>') 
+        self.button_prev.clicked.connect(self.on_prev)
+        self.button_next.clicked.connect(self.on_next)
+        self.choice_note=QtGui.QComboBox()
+        self.choice_note.addItems(['0','1','2','3','4','5'])
+        self.choice_note.activated.connect(self.on_marks_change)
+        self.choice_note2=QtGui.QComboBox()
+        self.choice_note2.addItems(['0','1','2','3','4','5'])
+        self.choice_note2.setEnabled(False)
+        self.choice_note2.currentIndexChanged.connect(self.on_marks_change2)
+        self.comment=QtGui.QTextEdit()
+        currentSize = self.comment.size()
+        self.comment.setMaximumSize(500,50)
+        self.text_in_comment=QtCore.QString()
+        self.comment.textChanged.connect(self.on_comment_change)
+        
+        self.display_xls=QtGui.QPushButton('Display_xls')
+        self.xls_comment=QtGui.QTextEdit()
+        self.xls_comment.setReadOnly(True)
+        self.xls_comment.setVisible(False)     
+        self.hbox.addWidget(self.button_prev)
+        self.hbox.addWidget(self.button_next)
+        self.hbox.addWidget(QtGui.QLabel('Grade Image Display'))         
+        self.hbox.addWidget(self.choice_note)
+        self.hbox.addWidget(QtGui.QLabel('Grade Brain Mask')) 
+        self.hbox.addWidget(self.choice_note2)   
+        self.hbox.addWidget(self.comment)
+        self.hbox.addWidget(self.display_xls)      
+        self.vbox.addLayout(self.hbox)
+        
+         #Image
+        self.scene=QtGui.QGraphicsScene()
+        self.view=QtGui.QGraphicsView(self.scene)
+        self.view.setDragMode(QtGui.QGraphicsView.ScrollHandDrag)
+        
+        self.view.wheelEvent = self.wheel_event
+        #self.picture_display=QGraphicsPixmapItem()
+        #self.scene.addItem(self.picture_display)
+        self.vbox.addWidget(self.view)
+        self.mousePressEvent = self.back_focus
+        self.MainWidget.setLayout(self.vbox)
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.setCentralWidget(self.MainWidget)
+      
+    def back_focus(self,event):
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)  
+
+    def create_menu(self):
+        menubar = self.menuBar()
+        filemenu=menubar.addMenu('&File')
+        filemenu.addAction(self.action_open)
+        filemenu.addAction(self.action_open_xls)
+        filtermenu=menubar.addMenu('&Filter')
+        filtermenu.addAction(self.action_filter)
+
+     
+    def create_actions(self):
+        #Action filemenu
+        self.action_open=QtGui.QAction('Open',self)
+        self.action_open.triggered.connect(self.on_open)
+        self.action_open_xls=QtGui.QAction('Open Xls',self)
+        self.action_open_xls.triggered.connect(self.on_open_xls)
+        #Action filtermenu
+        self.action_filter=QtGui.QAction('Launch filter',self)
+        self.action_filter.triggered.connect(self.on_open_filter)
+
+
+    def resizeEvent(self,resizeEvent):
+        if self.data_file_name is not None:
+            self.open_image(self.pictures_in_directory[self.index_picture_display]) 
+
+    def keyPressEvent(self, event):
+        if self.data_file_name is not None:
+            if event.key() == QtCore.Qt.Key_Left:
+                print "Left touch keyboard"
+                self.on_prev()
+            if event.key() == QtCore.Qt.Key_Right: 
+                print "Right touch keyboard"
+                self.on_next()
+         
+   
+    def on_next(self):
+        print 'on next' 
+        if self.data_file_name is not None:
+            if (self.index_picture_display+1)==len(self.pictures_in_directory):
+                self.index_picture_display=0
+            else:
+                self.index_picture_display=self.index_picture_display+1    
+           
+            self.open_image(self.pictures_in_directory[self.index_picture_display]) 
+            self.statusBar().showMessage(self.pictures_in_directory[self.index_picture_display])
+            self.find_picture_split()
+            self.check_data()  
+        
+    def on_prev(self):
+        print 'on prev'   
+        if self.data_file_name is not None:
+            if (self.index_picture_display-1)==-1:
+                self.index_picture_display=len(self.pictures_in_directory)-1
+            else:
+                self.index_picture_display=self.index_picture_display-1
+             
+            self.open_image(self.pictures_in_directory[self.index_picture_display])     
+            self.statusBar().showMessage(self.pictures_in_directory[self.index_picture_display])             
+            self.find_picture_split()
+            self.check_data() 
+            
+    def on_marks_change(self):
+        print 'marks change'
+        if self.data_file_name is not None:
+            if self.choice_note2.isEnabled() is True:
+                self.data.set_double_note(self.pictures_in_directory[self.index_picture_display],self.choice_note.currentIndex(),self.choice_note2.currentIndex())
+            else:
+                self.data.set_simple_note(self.pictures_in_directory[self.index_picture_display],self.choice_note.currentIndex())
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)               
+                               
+    def on_marks_change2(self):
+        print 'brain marks change'
+        if self.data_file_name is not None:
+                self.data.set_double_note(self.pictures_in_directory[self.index_picture_display],self.choice_note.currentIndex(),self.choice_note2.currentIndex())
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)
+        
+        
+    def on_comment_change(self):
+        print 'comment change'
+        if self.data_file_name is not None:
+            #print self.comment.toPlainText()
+            #print str(self.comment.toPlainText())
+            #print self.comment.toPlainText().toUtf8()
+            #print type(self.comment.toPlainText()),self.comment.toPlainText()
+            self.text_in_comment=self.comment.toPlainText()
+            #print type(self.text_in_comment),self.text_in_comment
+            self.text_in_comment=self.comment.toPlainText().toUtf8()
+            #print type(self.text_in_comment),self.text_in_comment
+            self.data.set_comment(self.pictures_in_directory[self.index_picture_display],self.text_in_comment)
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)
+
+            
+                
+    def on_open(self):
+        print 'in open'      
+        fname = QtGui.QFileDialog.getOpenFileName(self, 'Open file','',self.tr("Images Files (*.png);;(*jpg)"))
+        if fname != '':
+            #self.picture_display.setPixmap(QPixmap(fname))
+            self.open_image(fname)
+            self.dirname=os.path.dirname(str(fname))
+            self.data_file_name = os.path.join(self.dirname,'data.csv')
+            results = [each for each in os.listdir(self.dirname) if each.endswith('.png')]
+            results.sort()
+            self.pictures_in_directory=results
+            i=-1
+            for picture in self.pictures_in_directory:
+                i=i+1
+                if picture==os.path.basename(str(fname)):
+                    self.index_picture_display=i
+                    break        
+                         
+        if self.data_file_name is not None:    
+         #Display image       
+            self.statusBar().showMessage(self.pictures_in_directory[self.index_picture_display])
+            self.data.load(self.data_file_name)
+            self.find_picture_split()
+            self.check_data()
+            self.statusBar().showMessage(self.pictures_in_directory[self.index_picture_display])             
+
+            #self.size_image()
+        #Get back focus 
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)    
+            
+    def open_image(self, path):
+        print 'open image'
+        w_vue, h_vue = self.view.width(), self.view.height() 
+        self.current_image = QtGui.QImage(path)
+        self.pixmap = QtGui.QPixmap.fromImage(self.current_image.scaled(w_vue, h_vue,
+                                    QtCore.Qt.KeepAspectRatio, 
+                                    QtCore.Qt.SmoothTransformation)) 
+        self.view_current()
+
+    def view_current(self):
+        w_pix, h_pix = self.pixmap.width(), self.pixmap.height()
+        self.scene.clear()
+        self.scene.setSceneRect(0, 0, w_pix, h_pix)
+        self.scene.addPixmap(self.pixmap)
+        #self.view.setScene(self.scene)
+        
+    def wheel_event (self, event):
+        if self.data_file_name is not None:
+            steps = event.delta() / 120.0
+            self.zoom(steps)
+            event.accept()
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)   
+    
+    def zoom(self, step):
+        w_pix, h_pix = self.pixmap.width(), self.pixmap.height()
+        w, h = w_pix * (1 + 0.1*step), h_pix * (1 + 0.1*step)
+        self.pixmap = QtGui.QPixmap.fromImage(self.current_image.scaled(w, h, 
+                                            QtCore.Qt.KeepAspectRatio, 
+                                            QtCore.Qt.FastTransformation))
+        self.view_current()
+
+            
+            
+    def on_open_xls (self):
+        print 'on open xls - On working'    
+        #Get back focus 
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)        
+            
+            
+    def on_open_filter(self):
+        print 'on open filter'
+        if self.data_file_name is not None:
+            self.data.save(self.data_file_name,self.pictures_in_directory)  
+            setattr(self.filter_window,'dirname_snap',self.dirname) 
+            self.filter_window.open()
+        else:
+            print 'PLEASE OPEN AN IMAGE BEFORE USING FILTER'          
+    
+        #Get back focus 
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)      
+            
+    def check_brain_exist(self):       
+        word_to_find='brain'
+        self.image_brain=self.pictures_in_directory[self.index_picture_display].replace('split','brain')
+        element=[]
+        element+= [image for image in self.pictures_in_directory if image==self.image_brain]
+        if not element:
+            return 0
+        else:
+            return 1        
+            
+    def check_data(self):
+        if self.data.is_recorded(self.pictures_in_directory[self.index_picture_display])==0:
+            self.data.add_filename(self.pictures_in_directory[self.index_picture_display])
+            self.data.save(self.data_file_name,self.pictures_in_directory)          
+            if self.choice_note2.isEnabled() is True:
+                self.choice_note.setCurrentIndex(self.data.get_double_note(self.pictures_in_directory[self.index_picture_display],False))
+                self.choice_note2.setCurrentIndex(self.data.get_double_note(self.pictures_in_directory[self.index_picture_display],True))
+            else:
+                self.choice_note.setCurrentIndex(self.data.get_simple_note(self.pictures_in_directory[self.index_picture_display]))                        
+            self.comment.setPlainText(self.data.get_comment(self.pictures_in_directory[self.index_picture_display]))
+        
+        else:
+            self.text_in_comment=QtCore.QString.fromUtf8(self.data.get_comment(self.pictures_in_directory[self.index_picture_display]))
+            if self.choice_note2.isEnabled() is True:
+                self.choice_note.setCurrentIndex(self.data.get_double_note(self.pictures_in_directory[self.index_picture_display],False))
+                self.choice_note2.setCurrentIndex(self.data.get_double_note(self.pictures_in_directory[self.index_picture_display],True))
+            else:   
+                self.choice_note.setCurrentIndex(self.data.get_simple_note(self.pictures_in_directory[self.index_picture_display]))   
+            self.comment.setPlainText(unicode(self.text_in_comment))
+            self.data.save(self.data_file_name,self.pictures_in_directory)    
+            
+            
+    def find_picture_split(self):
+        word_to_find='split'
+        if word_to_find in self.pictures_in_directory[self.index_picture_display]:
+            self.choice_note2.setEnabled(True)
+        else:
+            self.choice_note2.setEnabled(False)
+
+
+
+    def closeEvent(self, event): 
+        self.data.save(self.data_file_name,self.pictures_in_directory)    
+
