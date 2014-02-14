3d25da41d5995f880dedf241237be84cfb607828
denisri <denis.riviere.gm@gmail.com>
2014-02-14 18:04:52 +0000

    sphinx docs: make use of numpydoc optional if it is not installed.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/soma/soma-base/trunk@60980 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -23,7 +23,12 @@ import soma.config
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', 'sphinx.ext.extlinks', 'numpydoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', 'sphinx.ext.extlinks']
+try:
+    import numpydoc
+    extensions.append('numpydoc')
+except:
+    pass # numpydoc is not here, don't use it
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
