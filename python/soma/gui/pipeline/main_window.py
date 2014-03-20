bb7b9c8dc011ff6af18cca43a0b8b538cfcaef67
denisri <denis.riviere.gm@gmail.com>
2014-03-20 17:02:58 +0000

    capsul/soma switches
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/soma/soma-base/trunk@61531 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/soma/gui/pipeline/main_window.py
+++ python/soma/gui/pipeline/main_window.py
@@ -5,8 +5,10 @@ from soma.gui.pipeline.display_bdd import DisplayBDD
 from soma.application import Application 
 try:
   from capsul.process import Process, get_process_instance
+  print '%s uses CAPSUL.' % __name__
 except ImportError:
   from soma.process import Process, get_process_instance
+  print '%s uses Soma.' % __name__
 from soma.pipeline.process_with_fom import ProcessWithFom
 from soma.pipeline.study import Study
 from soma.gui.pipeline.process_gui import ProcessGui
@@ -24,16 +26,16 @@ class MainWindow(QtGui.QMainWindow):
         super(MainWindow, self).__init__()
         self.main_widget=QtGui.QWidget()
         self.vbox=QtGui.QVBoxLayout()
-        
+
         ##Study
         self.btn_get_study=QtGui.QPushButton('Get .json from study')
         self.vbox.addWidget(self.btn_get_study)
-        
+
         self.vbox.addWidget(QtGui.QLabel('Study Information'))
         # Find foms available
-        foms = Application().fom_manager.find_foms()        
+        foms = Application().fom_manager.find_foms()
         #self.grid=QtGui.QGridLayout()
-      
+
         #Scroll area to show completion
         self.scroll_area = QtGui.QScrollArea( parent=self )
         self.scroll_area.setWidgetResizable( True )
@@ -42,7 +44,7 @@ class MainWindow(QtGui.QMainWindow):
         #Create controller widget for process and object_attribute
         self.controller_widget22 = ControllerWidget( Study.get_instance(), live=True, parent=self.scroll_area )
         self.scroll_area.setWidget( self.controller_widget22 )
-      
+
 
         #Launch Simple process
         self.btn_run_simple_process=QtGui.QPushButton('GUI Simple Process')
@@ -52,7 +54,7 @@ class MainWindow(QtGui.QMainWindow):
         self.btn_run_iteration_process=QtGui.QPushButton('GUI Iteration Process')
         #Launch display BDD
         self.btn_display_bdd=QtGui.QPushButton('Launch BDD')
-    
+
         self.vbox.addWidget(self.btn_run_simple_process)
         self.vbox.addWidget(self.btn_run_pipeline_process)
         self.vbox.addWidget(self.btn_run_iteration_process)
@@ -62,7 +64,7 @@ class MainWindow(QtGui.QMainWindow):
         self.main_widget.setLayout(self.vbox)
         self.setCentralWidget(self.main_widget)
 
-    """Create widgets signals """   
+    """Create widgets signals """
     def signals(self):
         self.btn_get_study.clicked.connect(self.on_get_study)
         self.btn_run_simple_process.clicked.connect(self.run_simple_process)
@@ -70,17 +72,17 @@ class MainWindow(QtGui.QMainWindow):
         self.btn_run_iteration_process.clicked.connect(self.run_iteration_process) 
         self.btn_display_bdd.clicked.connect(self.on_display_bdd)
 
-   
+
     def on_get_study(self):
         name_json = QtGui.QFileDialog.getOpenFileName (self, 'Select a .json study','/home', '*.json')
         if name_json:
             Study.get_instance().load(name_json)
-   
-   
+
+
 
     def run_simple_process(self):
         """Launch simple process"""
-        print 'RUN SIMPLE PROCESS'        
+        print 'RUN SIMPLE PROCESS'
         #FIXME utile je pense pour creer fichier json si existe pas..
         Study.get_instance().save()
         process_specific=self.get_object_process()
@@ -89,16 +91,16 @@ class MainWindow(QtGui.QMainWindow):
         process_with_fom=ProcessWithFom(process_specific)
         self.process_gui=ProcessGui(process_with_fom,process_specific)
         self.process_gui.open()
-        
-        
+
+
     def run_pipeline_process(self):
         """Launch pipeline process"""
         subprocess.check_call(['python', '-m', 'morphologist.process'])
 
 
-        
 
-    
+
+
     def run_iteration_process(self):
         """Launch iteration process"""
         print 'RUN ITERATION PROCESS' 
@@ -114,7 +116,7 @@ class MainWindow(QtGui.QMainWindow):
         self.wizard.addPage(self.second_page)
         self.wizard.show()
 
-        
+
     def on_page_changed(self,ide):
         if ide==1:
             ##Check if subjects have selected
@@ -123,35 +125,35 @@ class MainWindow(QtGui.QMainWindow):
                 self.second_page.go()
             elif self.second_page.list_subjects_selected == self.first_page.list_subjects_selected:
                 print 'NO SUBJECTS ADD'
-        
+
             else:
                 self.add_element(self.first_page.list_subjects_selected,self.second_page.list_subjects_selected)
                 self.del_element(self.first_page.list_subjects_selected,self.second_page.list_subjects_selected)
                 self.second_page.list_subjects_selected=self.first_page.list_subjects_selected[:]
-        
-        
-        
+
+
+
     def add_element(self,new_list,prec_list):
         list_add_element=[x for x in new_list if x not in prec_list]
         if list_add_element:
             self.second_page.add_element_on_table(list_add_element)
-           
+
 
     def del_element(self,new_list,prec_list):
         list_del_element=[x for x in prec_list if x not in new_list]
         if list_del_element:
             for ele in list_del_element:
                 self.second_page.del_element_on_table(ele)
-    
-        
-    
+
+
+
     def on_display_bdd(self):
         print 'on display bdd'
         import morphologistSimp
         process_specific = self.get_object_process()
         self.display_bdd=DisplayBDD(process_specific)
         self.display_bdd.open()
-        
+
 
     def get_object_process(self):
         """This will be automatic"""
