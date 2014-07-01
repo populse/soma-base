ca09aa613c48812ccb6f3cb0d0bcc47fd74e657e
denisri <denis.riviere.gm@gmail.com>
2014-07-01 13:48:46 +0000

    info.py update
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/soma/soma-base/trunk@62738 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/soma/info.py
+++ python/soma/info.py
@@ -28,50 +28,36 @@ CLASSIFIERS = ["Development Status :: 1 - Planning",
                "Topic :: Scientific/Engineering",
                "Topic :: Utilities"]
 
-description = 'CASPER'
+description = 'Soma-base'
 
 long_description = """
-========
-SOMA 
-========
+=========
+SOMA-BASE
+=========
 
-[soma] CollAborative System for Platform Engineering and Reasearch.
-SOMA pipeline offers a catalogue of tools mastered by the platform team.
+Miscelaneous all-purpose classes and functions in Python.
+
 """
 
 # versions for dependencies
-SPHINX_MIN_VERSION = 1.0
-NIBABEL_MIN_VERSION = '1.3.0'
-NUMPY_MIN_VERSION = '1.3'
-SCIPY_MIN_VERSION = '0.7.2'
-PYDICOM_MIN_VERSION = '0.9.7'
-NIPYPE_VERSION = '0.8.0'
-MATPLOTLIB_MIN_VERSION = '1.1.1rc'
+SPHINX_MIN_VERSION = '1.0'
 
 # Main setup parameters
-NAME = 'soma'
+NAME = 'soma-base'
 ORGANISATION = "CEA"
-MAINTAINER = "Antoine Grigis"
-MAINTAINER_EMAIL = "antoine.grigis@cea.fr"
+MAINTAINER = "CEA"
+MAINTAINER_EMAIL = ""
 DESCRIPTION = description
 LONG_DESCRIPTION = long_description
 URL = ""
 DOWNLOAD_URL = ""
 LICENSE = "CeCILL-B"
 CLASSIFIERS = CLASSIFIERS
-AUTHOR = "CASPER developers"
-AUTHOR_EMAIL = "antoine.grigis@cea.fr"
+AUTHOR = "CEA"
+AUTHOR_EMAIL = ""
 PLATFORMS = "OS Independent"
-MAJOR = _version_major
-MINOR = _version_minor
-MICRO = _version_micro
 ISRELEASE = _version_extra == ''
 VERSION = __version__
-PROVIDES = ["soma"]
-REQUIRES = ["numpy>={0}".format(NUMPY_MIN_VERSION),
-            "scipy>={0}".format(SCIPY_MIN_VERSION),
-            "matplotlib>={0}".format(MATPLOTLIB_MIN_VERSION),
-            "pydicom>={0}".format(PYDICOM_MIN_VERSION),
-            "nibabel>={0}".format(NIBABEL_MIN_VERSION),
-            "nipype=={0}".format(NIPYPE_VERSION)]
-EXTRA_REQUIRES = {"doc": ["sphinx>=1.0"]}
+PROVIDES = ["soma-base"]
+REQUIRES = []
+EXTRA_REQUIRES = {"doc": ["sphinx>=%s" % SPHINX_MIN_VERSION]}
