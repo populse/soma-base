79e70c7ec27bbe17aaa1eb42439ef0770698ca07
denisri <denis.riviere.gm@gmail.com>
2014-07-03 12:39:27 +0000

    fixed unconditional import of enthought.traits
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/soma/soma-base/trunk@62818 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/soma/gui/widget_factory.py
+++ python/soma/gui/widget_factory.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from enthought.traits.api import HasTraits
+try:
+    from traits.api import HasTraits
+except:
+    from enthought.traits.api import HasTraits
 from soma.qt_gui.qt_backend import set_qt_backend, loadUi
 from soma.factory import Factories
 from soma.controller import Controller
