5a9ee7eb618e24865357a967d3dd14eb341836f9
sapetnioc <yann@cointepas.net>
2015-04-08 10:30:14 +0000

    soma-base cleanup
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/soma/soma-base/trunk@66447 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/test/test_soma_base.py
+++ python/test/test_soma_base.py
@@ -1,12 +1,3 @@
-#! /usr/bin/env python
-##########################################################################
-# CAPSER - Copyright (C) CEA, 2013
-# Distributed under the terms of the CeCILL-B license, as published by
-# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
-# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
-# for details.
-##########################################################################
-
 from load_pilots import load_pilots
 import soma
 
