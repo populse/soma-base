cb5d5b15f6ac7220e86c7124b8f393a4159ca2d8
duboism <duboism@users.noreply.github.com>
2017-05-14 20:00:20 +0000

    [MISC] Use absolute import.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/soma/soma-base/branches/bug_fix@77588 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/soma/test_utils/__init__.py
+++ python/soma/test_utils/__init__.py
@@ -1,4 +1,4 @@
-from base_classes import test_modes, run_mode, ref_mode, default_mode, \
+from .base_classes import test_modes, run_mode, ref_mode, default_mode, \
      SomaTestLoader, SomaTestCase
 
 __all__ = [test_modes, run_mode, ref_mode, default_mode, SomaTestLoader,
