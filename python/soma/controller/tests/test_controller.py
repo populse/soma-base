f7a04a9f65fe8bccafe33ba17c8737a3a67c756f
denisri <denis.riviere.gm@gmail.com>
2018-11-12 17:35:18 +0000

    improved tests
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/soma/soma-base/branches/bug_fix@83763 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/soma/controller/tests/test_controller.py
+++ python/soma/controller/tests/test_controller.py
@@ -5,7 +5,7 @@ import unittest
 import shutil
 import os
 import tempfile
-from soma.controller import Controller
+from soma.controller import Controller, ControllerTrait, OpenKeyController
 import traits.api as traits
 
 
@@ -21,6 +21,12 @@ class TestController(unittest.TestCase):
         self.assertEqual(c1.user_traits().keys(), ['gogo', 'bozo'])
         c1.gogo = 'blop krok'
         self.assertEqual(c1.gogo, 'blop krok')
+        d = c1.export_to_dict()
+        self.assertEqual(d, {'gogo': 'blop krok', 'bozo': 12})
+        c1.reorder_traits(['bozo', 'gogo'])
+        self.assertEqual(c1.user_traits().keys(), ['bozo', 'gogo'])
+        c1.reorder_traits(['gogo', 'bozo'])
+        self.assertEqual(c1.user_traits().keys(), ['gogo', 'bozo'])
 
     def test_controller2(self):
         class Zuzur(Controller):
@@ -33,6 +39,81 @@ class TestController(unittest.TestCase):
         c3.glop = 'I am c3'
         self.assertEqual(c2.glop, 'I am c2')
         self.assertEqual(c3.glop, 'I am c3')
+
+    def test_controller3(self):
+        class Babar(Controller):
+            hupdahup = traits.Str('barbatruc')
+            gargamel = traits.Str(traits.Undefined)
+            ouioui = traits.List(traits.Str())
+
+        c1 = Babar()
+        self.assertEqual(c1.gargamel, traits.Undefined)
+        d = c1.export_to_dict()
+        self.assertEqual(d, {'hupdahup': 'barbatruc',
+                             'gargamel': traits.Undefined,
+                             'ouioui': []})
+        c2 = Babar()
+        c2.gargamel = 'schtroumpf'
+        c2.import_from_dict(d)
+        self.assertEqual(c2.export_to_dict(), d)
+        d = c1.export_to_dict(exclude_undefined=True)
+        self.assertEqual(d, {'hupdahup': 'barbatruc', 'ouioui': []})
+        c2.gargamel = 'schtroumpf'
+        c2.import_from_dict(d)
+        self.assertEqual(c2.export_to_dict(exclude_empty=True),
+                         {'hupdahup': 'barbatruc', 'gargamel': 'schtroumpf'})
+
+    def test_controller4(self):
+        class Driver(Controller):
+            head = traits.Str()
+            arms = traits.Str()
+            legs = traits.Str()
+
+        class Car(Controller):
+            wheels = traits.Str()
+            engine = traits.Str()
+            driver = ControllerTrait(Driver())
+            problems = ControllerTrait(OpenKeyController(traits.Str()))
+
+        my_car = Car()
+        my_car.wheels = 'flat'
+        my_car.engine = 'wind-broken'
+        my_car.driver.head = 'empty' # ! modify class trait !
+        my_car.driver.arms = 'heavy'
+        my_car.driver.legs = 'short'
+        my_car.problems = {'exhaust': 'smoking', 'windshield': 'cracked'}
+
+        d = my_car.export_to_dict()
+        self.assertEqual(d, {'wheels': 'flat', 'engine': 'wind-broken',
+                             'driver': {'head': 'empty', 'arms': 'heavy',
+                                        'legs': 'short'},
+                             'problems': {'exhaust': 'smoking',
+                                          'windshield': 'cracked'}})
+        self.assertTrue(isinstance(my_car.driver, Driver))
+        self.assertTrue(isinstance(my_car.problems, OpenKeyController))
+        my_car.driver = {'head': 'smiling', 'legs': 'strong'}
+        d = my_car.export_to_dict()
+        self.assertEqual(d, {'wheels': 'flat', 'engine': 'wind-broken',
+                             'driver': {'head': 'smiling', 'arms': '',
+                                        'legs': 'strong'},
+                             'problems': {'exhaust': 'smoking',
+                                          'windshield': 'cracked'}})
+
+        other_car = my_car.copy(with_values=True)
+        self.assertEqual(other_car.export_to_dict(), d)
+        other_car = my_car.copy(with_values=False)
+        self.assertEqual(other_car.export_to_dict(),
+                         {'wheels': '', 'engine': '',
+                          'driver': {'head': 'empty', 'arms': 'heavy',
+                                     'legs': 'short'},
+                          'problems': {}})
+
+        self.assertRaises(traits.TraitError,
+                          setattr, my_car.problems, 'fuel', 3.5)
+        del my_car.problems.fuel
+        self.assertEqual(sorted(my_car.problems.user_traits().keys()),
+                         ['exhaust', 'windshield'])
+
 
 
 def test():
