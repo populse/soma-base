30d593b7c1acb9513c4b1f3d6e131411e58e4d85
Denis Rivière <denis.riviere.gm@gmail.com>
2022-02-22 19:08:12 +0100

    fix for python 3.8

--- python/soma/controller/controller.py
+++ python/soma/controller/controller.py
@@ -296,8 +296,8 @@ class Controller(metaclass=ControllerMeta, ignore_metaclass=True):
         else:
             field = self.__dataclass_fields__[name]
             type_ = field.type.__args__[0]
-            print('type:', type_, type(type))
-            if isinstance(value, dict) and issubclass(type_, Controller):
+            if isinstance(value, dict) and isinstance(type_, type) \
+                    and issubclass(type_, Controller):
                 controller = getattr(self, name, undefined)
                 if controller is undefined:
                     controller = type_()
