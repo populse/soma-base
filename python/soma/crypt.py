a7a3089a7735f0714f5ddbda47a8a3c9bf773dc8
Denis Rivière <denis.riviere.gm@gmail.com>
2022-11-14 13:54:15 +0100

    fix after changes in Crypt API

--- python/soma/crypt.py
+++ python/soma/crypt.py
@@ -6,6 +6,7 @@ This module needs Crypto module.
 
 from __future__ import absolute_import
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 from base64 import b64decode, b64encode
 import Crypto
 import six
@@ -47,7 +48,9 @@ def encrypt_RSA(public_key_loc, message):
     rsakey = RSA.importKey(key)
     if not isinstance(message, six.binary_type):
         message = message.encode()
-    encrypted = rsakey.encrypt(message, None)[0]
+    encryptor = PKCS1_OAEP.new(rsakey)
+    encrypted = encryptor.encrypt(message)
+    #encrypted = rsakey.encrypt(message, None)[0]
     return b64encode(encrypted)
 
 
@@ -59,5 +62,7 @@ def decrypt_RSA(private_key_loc, package):
     '''
     key = open(private_key_loc, "rb").read()
     rsakey = RSA.importKey(key)
-    decrypted = rsakey.decrypt(b64decode(package))
+    decryptor = PKCS1_OAEP.new(rsakey)
+    decrypted = decryptor.decrypt(b64decode(package))
+    #decrypted = rsakey.decrypt(b64decode(package))
     return decrypted
