090b2cc23eb46a7e0e7a2859cfd925188831e2fe
Denis Rivière <denis.riviere.gm@gmail.com>
2022-12-12 17:18:53 +0100

    allow to specify fixed attibutes (dict) in csv_utils.dict_to_table()

--- python/soma/utils/csv_utils.py
+++ python/soma/utils/csv_utils.py
@@ -32,6 +32,30 @@ def dict_to_table(data_dict, shape_dict):
          [0, 7, 1300.6187744140625, 323.5833],
          [0, 8, None, 103.076324],
          [1, 2, 350.5135, None]]
+
+    At each level of the data dict, some additional, fixed, attributes, may
+    be specified in an optional "fixed_attributes" sub-dict. Ex::
+
+        data_dict = {
+            0: {'areas': {0: 23483.013671875,
+                          2: 325.46331787109375,
+                          7: 1300.6187744140625},
+                'lengths': {0: 2225.728,
+                            2: 80.755554,
+                            7: 323.5833,
+                            8: 103.076324}},
+            1: {'areas': {2: 350.5135}},
+            'fixed_attributes': {'subject': 'maman'},
+        }
+
+    will output (with the same shape_dict)::
+
+        [['subject', 'timestep', 'parcel', 'area', 'length'],
+         ['maman', 0, 0, 23483.013671875, 2225.728],
+         ['maman', 0, 2, 325.46331787109375, 80.755554],
+         ['maman', 0, 7, 1300.6187744140625, 323.5833],
+         ['maman', 0, 8, None, 103.076324],
+         ['maman', 1, 2, 350.5135, None]]
     '''
 
     todo = [(data_dict, shape_dict, {})]
@@ -39,6 +63,10 @@ def dict_to_table(data_dict, shape_dict):
     while todo:
         data, shape, keys = todo.pop(0)
         if isinstance(data, dict):
+            if 'fixed_attributes' in data:
+                keys.update(data['fixed_attributes'])
+                data = dict(data)
+                del data['fixed_attributes']
             if len(shape) == 1:
                 key, sshape = next(iter(shape.items()))
                 for k, v in data.items():
