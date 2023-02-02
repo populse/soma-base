d97cd72ca48b7bcb43cdd300a0d61f07a8696d5d
sapetnioc <sapetnioc@users.noreply.github.com>
2023-02-02 15:00:44 +0100

    #25

--- python/soma/info.py
+++ python/soma/info.py
@@ -12,8 +12,8 @@ version_extra
 from __future__ import absolute_import
 import sys
 
-version_major = 5
-version_minor = 1
+version_major = 6
+version_minor = 0
 version_micro = 0
 version_extra = ''
 _version_major = version_major
