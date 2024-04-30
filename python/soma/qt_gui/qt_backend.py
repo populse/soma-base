87da0dc3df561fe8e825a1f892a67fae63fa3f10
econdami <econdami@yahoo.fr>
2024-04-30 14:06:35 +0200

    updated advanced customization of QT module import process

--- python/soma/qt_gui/qt_backend.py
+++ python/soma/qt_gui/qt_backend.py
@@ -63,7 +63,7 @@ make_compatible_qt5 = False
 
 class QtImporter(object):
 
-    def find_module(self, fullname, path=None):
+    def find_spec(self, fullname, path=None, target=None):
         modsplit = fullname.split('.')
         modpath = '.'.join(modsplit[:-1])
         module_name = modsplit[-1]
@@ -91,14 +91,23 @@ class QtImporter(object):
                 try:
                     found = importlib.util.find_spec(module_name)
                     if found is not None:
-                        return self
+                        found.loader = self
+                        return found
                     else:
                         return None
                 except ImportError:
                     return None
-        return self
+        found.loader = self
+        return found
 
-    def load_module(self, name):
+    def exec_module(self, module):
+        # this method can be used to execute the module in the module
+        # namespace, which can include adding variables, functions, etc.
+        pass
+
+    def create_module(self, spec=None, name=None):
+        if name is None:
+            name = spec.name
         qt_backend = get_qt_backend()
         module_name = name.split('.')[-1]
         imp_module_name = module_name
@@ -144,7 +153,7 @@ class QtImporter(object):
                 mods.remove('Qt')
             for mod in mods:
                 try:
-                    psmods.append(self.load_module('.'.join(base + [mod])))
+                    psmods.append(self.create_module(name='.'.join(base + [mod])))
                 except ImportError:
                     pass
             patch_main_modules(psmods)
