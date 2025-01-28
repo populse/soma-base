558b5474fadb9b1c9ba0c309273a7f6bf8268ffe
Denis Rivière <denis.riviere.gm@gmail.com>
2025-01-28 17:08:46 +0100

    a different fix for closing files in minf writer
    previous fix was broken

--- python/soma/minf/writer.py
+++ python/soma/minf/writer.py
@@ -133,7 +133,6 @@ class MinfWriter(six.with_metaclass(RegisterMinfWriterClass, object)):
                                         for i in
                                         MinfWriter._allWriterClasses])})
         if not hasattr(destFile, 'write'):
-            with open(destFile, 'w') as destFile:
-                return writer(destFile, reducer, )
+            destFile = open(destFile, 'w')
         return writer(destFile, reducer, )
     createWriter = staticmethod(createWriter)
