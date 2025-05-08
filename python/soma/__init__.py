2961ef692633facebc52a8fb9f920e0c8dee85f4
sapetnioc <sapetnioc@users.noreply.github.com>
2025-05-08 09:28:15 +0200

    Added soma.short_version

--- python/soma/__init__.py
+++ python/soma/__init__.py
@@ -11,8 +11,10 @@ except importlib.metadata.PackageNotFoundError:
 
 if __release__:
     __version__ = re.match(r"(\d+\.\d+\.\d+)[^.\d]*", __release__).group(1)
+    short_version = ".".join(__version__.split(".")[:2])
 else:
     __version__ = None
+    short_version = None
 
 # Enable from soma import aims
 if "PIXI_PROJECT_ROOT" in os.environ:
